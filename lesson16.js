ec8a7669bc8d172adec240bcf989366b15172177
AllayarovQuldosh <94292201+AllayarovQuldosh@users.noreply.github.com>
2021-11-26 07:32:37 -0500

    Add files via upload

---
+++ lesson16.js
@@ -0,0 +1,91 @@
+// >>>>>>>>>>>>>>>> Promise 5ta  Anync 5ta misol yechish
+
+// Promise.resolve('I am Promise !!!')
+// .then((v) => {
+//     console.log(v);
+// })
+
+// function promise(){
+//     console.log('I am promisess !!!');
+//     return 'I am Promise !!!'
+// }
+// // let pr = promise();
+
+
+// async function asyncFunc(){
+//     // let af = await promise()
+//     // console.log(af);
+//     return 'I am async Function !!!';
+// }
+// console.log(asyncFunc());
+
+// asyncFunc().then((value)=>{  
+//     console.log(value);
+// })
+
+
+
+// // >>>>>>>>>>> Promise
+
+// function promiseFunc() {
+//     return new Promise((resolve, reject) => {
+//         resolve('Hello I m promiseFunc')
+//     })
+// }
+// async function asyncFunc() {
+//     return 'Hello I am asyncFunc'
+// }
+
+// // console.log(promiseFunc())
+// // console.log(asyncFunc())
+
+
+
+// promiseFunc().then(v => {
+//     console.log(v);
+// });
+
+// asyncFunc().then(v => {
+//     console.log(v);
+// })
+
+
+// // async function handler() {
+// //     let pr = await promiseFunc();
+// //     console.log(pr);
+// //     return pr;
+// // }
+
+// // // console.log(handler())
+
+// // handler().then((v) => {
+// //     console.log(v);
+// // })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
