2bd457dd9309817283399a30a342d5f9624c57ac
AllayarovQuldosh <94292201+AllayarovQuldosh@users.noreply.github.com>
2021-11-30 23:02:47 -0500

    Add files via upload

---
+++ repetition.js
@@ -0,0 +1,76 @@
+// >>>>>>>>> Repetition <<<<<<<<<<
+
+// class Person{
+//     constructor(name,age,address){
+//         this.n = name;
+//         this.a = age;
+//         this.add = address;
+//     }
+    
+//     getFullInfo(){
+//         return `Name: ${this.n}\nAge: ${this.a}\nAddress: ${this.add}`;
+//     }
+//     get changeName(){
+//         return this.n;
+//     }
+//     set changeName(n){
+//         this.n = n;
+//     }
+// }
+
+// const person = new Person('John',23,"Chilonzor");
+// person.n = 'Bernard';
+// console.log(person.getFullInfo());
+
+// class User{
+//     constructor(id,firstname,lastname,age,email,password){
+//         this.id = id;
+//         this._firstname = firstname;
+//         this._lastname = lastname;
+//         this.age = age;
+//         this._email = email;
+//         this._password = password;
+//     }
+//     set firstname(fi){
+//         this._firstname = fi;
+//     }
+//     set lastname(ls){
+//         this._lastname = ls;
+//     }
+//     set email(em){
+//         this._email = em;
+//     }
+//     set password(ps){
+//         this._password = ps;
+//     }
+// }
+
+// let user = new User(1,'John','Wick',33,'johnwick@gmail.com',11114444);
+// console.log(user);
+// user._password = 44441111;
+// user._firstname = 'Ali';
+// user._lastname = 'Valiyev';
+// user._email = 'alivaliyev@gmail.com';
+// console.log(user);
+
+// let arr = [
+//     {
+//         name: 'John',
+//         address: 'Chilonzor',
+//         location: {
+//             x:15,
+//             y:45
+//         }
+//     },
+//     'John',
+//     [1,2,3,4]
+// ]
+
+// let [a,{
+//     location: {
+//         x,
+//         y
+//     }
+// }] = arr;
+// console.log(x,y);
+
